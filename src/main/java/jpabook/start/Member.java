072cc7836c2992aef0b9aa55baf8893da7047530
Ingyu <gyu1039@naver.com>
2022-07-11 17:56:18 +0900

    연관 관계 매핑을 위한 Member, Team 클래스 생성 및 테스트 환경 구축

--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -6,15 +6,19 @@ import java.util.Date;
 @Entity
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
     @Column(name = "MEMBER_ID")
     private String id;
 
-    private String name;
+    private String username;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @ManyToOne
+    @JoinColumn(name="team_id")
+    private Team team;
+
+    public void setTeam(Team team) {
+        this.team =team;
+    }
 
     public String getId() {
         return id;
@@ -24,35 +28,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
